796db92d6df97c82d618d0d1afc1b8de594b5d6c
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2013-07-23 01:20:07 -0400

    Updated npm package

--- test/coral.js
+++ test/coral.js
@@ -112,7 +112,7 @@ describe('coral', function() {
   });
 
   it('version - must match version number', function() {
-    Coral.version.should.equal('0.1.4');
+    Coral.version.should.equal('0.1.5');
   });
 
 });
