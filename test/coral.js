57ad6a0ab8fc638f1479ea849e8fbea2d26ec52a
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2013-08-03 00:00:19 -0400

    Added idAttribute, findOne functionality, updated npm package

--- test/coral.js
+++ test/coral.js
@@ -112,7 +112,7 @@ describe('coral', function() {
   });
 
   it('version - must match version number', function() {
-    Coral.version.should.equal('0.1.6');
+    Coral.version.should.equal('0.1.7');
   });
 
 });
