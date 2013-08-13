223994424f6a366b38d09fa295d3dcea82d1a83d
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2013-08-13 01:34:06 -0400

    Updated npm package with subDoc functionality

--- test/coral.js
+++ test/coral.js
@@ -112,7 +112,7 @@ describe('coral', function() {
   });
 
   it('version - must match version number', function() {
-    Coral.version.should.equal('0.2.1');
+    Coral.version.should.equal('0.2.2');
   });
 
 });
