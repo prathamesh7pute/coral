b5c083e3e692110c155a743774e91d772d46ef87
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2014-07-13 17:46:07 -0400

    Added sub subDoc update functionality

---
+++ test/subDocQuery.update.js
@@ -0,0 +1,82 @@
+/**
+ * Test dependencies.
+ */
+var SubDocQuery = require('../lib/subDocQuery'),
+    db = require('./helper/db'),
+    should = require('should');
+
+describe('subDocQuery update tests', function() {
+    var subDocQuery;
+
+    before(function() {
+        db.connect();
+        subDocQuery = new SubDocQuery(db.getModel('Article'));
+    });
+
+    after(function(done) {
+        db.disconnect(done);
+    });
+
+    beforeEach(function(done) {
+        db.initialise(done);
+    });
+
+    it('update subDoc - must update record with chnaged value', function(done) {
+        var config = {
+            conditions: {
+                name: 'article-one'
+            },
+            subDoc: {
+                path: 'comments',
+                conditions: {
+                    name: 'comment-two'
+                }
+            }
+        };
+
+        var data = {
+            'body': 'Article One Second Comment - modified'
+        };
+
+        subDocQuery.findOneAndUpdate(config, data, function(err, record) {
+            record.name.should.equal('comment-two');
+            record.body.should.equal('Article One Second Comment - modified');
+            should.exist(record._id);
+            done();
+        });
+
+    });
+
+    it('update subDoc subDoc - must update record with chnaged value', function(done) {
+        var config = {
+            conditions: {
+                name: 'article-one'
+            },
+            subDoc: {
+                path: 'comments',
+                conditions: {
+                    name: 'comment-two'
+                },
+                subDoc: {
+                    path: 'replies',
+                    conditions: {
+                        name: 'reply-two'
+                    }
+                }
+            }
+        };
+
+        var data = {
+            'body': 'Article One Second Comment Second Reply - modified'
+        };
+
+        subDocQuery.findOneAndUpdate(config, data, function(err, record) {
+            record.name.should.equal('reply-two');
+            record.body.should.equal('Article One Second Comment Second Reply - modified');
+            should.exist(record._id);
+            done();
+        });
+
+    });
+
+});
